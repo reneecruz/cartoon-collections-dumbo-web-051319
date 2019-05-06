2d1e04e398188e3e9b1c67d543f60d1db42b7de8
Renée Cruz <reneebasscruz@gmail.com>
2019-05-06 23:26:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def find_the_cheese(contains_cheddar)
   
   cheese_types.each do |cheese| 
       if contains_cheddar.include?(cheese)
-          return cheese 
+        contains_cheddar.find(cheese)
       else cheese != cheese 
         return nil
       end
